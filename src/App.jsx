e5d2ec7f4d4a6a34ad27d68b86b8b182d3151074
Narendra <narendrach34@gmail.com>
2025-02-21 23:56:24 +0530

    del code-editor page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Editor from "./pages/Editor";
 import Login from "./pages/Login";
-import CodeEditor from "./pages/CodeEditor";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/editor/:roomId" element={<Editor />} />
-      <Route path="/code-editor" element={<CodeEditor />} />
     </Routes>
   );
 }
